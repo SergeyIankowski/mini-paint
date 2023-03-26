d2075a79932e33648c620c4a77e7fc1dffcf7f80
SeregeyIankowski <garret933@gmail.com>
2023-03-26 11:35:32 +0300

    feat: inplement AppRouter component and connect to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,18 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./AppRouter";
+import FirebaseContext from "./models/firebaseContext";
+import { auth, db } from "./firebase";
+import "./scss/main.scss";
+
 console.log("Hello World!");
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
+  <BrowserRouter>
+    <FirebaseContext.Provider value={{ db, auth }}>
+      <AppRouter />
+    </FirebaseContext.Provider>
+  </BrowserRouter>,
+);
