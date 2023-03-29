a0003ede4f47b8ac035511d420a33960547029d5
SeregeyIankowski <garret933@gmail.com>
2023-03-29 12:33:01 +0300

    feat: add actions for canvas color changing trough redux

--- src/store/reducers/toolSlice.ts
+++ src/store/reducers/toolSlice.ts
@@ -1,16 +1,30 @@
 /* eslint-disable no-param-reassign */
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import Tool from "../../tools/Tool";
 
 const initialState = {
-  tool: {},
+  tool: {
+    fillColor: "",
+    strokeColor: "",
+    lineWidth: 1,
+  },
 };
 
 export const toolSlice = createSlice({
   name: "currentDate",
   initialState,
   reducers: {
-    setTool(state, action: PayloadAction<string>) {
+    setTool(state, action: PayloadAction<Tool>) {
       state.tool = action.payload;
+    },
+    setFillColor(state, action: PayloadAction<string>) {
+      state.tool.fillColor = action.payload;
+    },
+    setStrokeColor(state, action: PayloadAction<string>) {
+      state.tool.strokeColor = action.payload;
+    },
+    setLineWidth(state, action: PayloadAction<number>) {
+      state.tool.lineWidth = action.payload;
     },
   },
 });
