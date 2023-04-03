23a7f9ae35a378ae8a3762828ce978a08fe62dd1
SeregeyIankowski <garret933@gmail.com>
2023-04-03 08:31:33 +0300

    fix: change styles in Spinner and Wrapper components

--- src/components/view/Wrapper/Wrapper.tsx
+++ src/components/view/Wrapper/Wrapper.tsx
@@ -1,9 +1,34 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
+import ThemeContext from "../../../context/Theme/ThemeContext";
 import WrapperProps from "./interface";
 import classes from "./wrapper.module.scss";
 
+const light = { backgroundColor: "#ffffff", color: "#000000" };
+const dark = { backgroundColor: "#808080", color: "white" };
+
+const setTheme = (str: string) => {
+  switch (str) {
+    case "light": {
+      return light;
+      break;
+    }
+    case "dark": {
+      return dark;
+      break;
+    }
+    default: {
+      return light;
+    }
+  }
+};
+
 const Wrapper: FC<WrapperProps> = ({ children }) => {
-  return <div className={classes.wrapper}>{children}</div>;
+  const { theme } = useContext(ThemeContext);
+  return (
+    <div className={`${classes.outerWrapper}`} style={setTheme(theme)}>
+      <div className={classes.innerWrapper}>{children}</div>
+    </div>
+  );
 };
 
 export default Wrapper;
