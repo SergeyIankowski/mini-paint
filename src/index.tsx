7f746d3c43739ce01c08dde0f7d2b397cc5ca118
SeregeyIankowski <garret933@gmail.com>
2023-04-03 08:48:53 +0300

    refactor: change project properties

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import AppRouter from "./AppRouter";
-import FirebaseContext from "./models/firebaseContext";
+import FirebaseContext from "./context/Firebase/firebaseContext";
 import { auth, db } from "./firebase";
 import "./scss/main.scss";
 import { setupStore } from "./store/store";
+import { ThemeProvider } from "./context/Theme/ThemeContext";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
@@ -15,7 +16,9 @@ root.render(
   <BrowserRouter>
     <Provider store={store}>
       <FirebaseContext.Provider value={{ db, auth }}>
-        <AppRouter />
+        <ThemeProvider>
+          <AppRouter />
+        </ThemeProvider>
       </FirebaseContext.Provider>
     </Provider>
   </BrowserRouter>,
